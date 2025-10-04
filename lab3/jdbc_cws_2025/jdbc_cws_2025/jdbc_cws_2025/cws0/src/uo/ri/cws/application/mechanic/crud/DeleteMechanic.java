dea772f403052d8e5fa13ddd5ad3b87a62452387
EloyRsua <eloy.rubio.suarez@gmail.com>
2025-10-04 10:08:48 +0200

    Añadir clase de borrar

--- lab3/jdbc_cws_2025/jdbc_cws_2025/jdbc_cws_2025/cws0/src/uo/ri/cws/application/mechanic/crud/DeleteMechanic.java
+++ lab3/jdbc_cws_2025/jdbc_cws_2025/jdbc_cws_2025/cws0/src/uo/ri/cws/application/mechanic/crud/DeleteMechanic.java
@@ -2,13 +2,19 @@ package uo.ri.cws.application.mechanic.crud;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import uo.ri.util.assertion.ArgumentChecks;
+import uo.ri.util.exception.BusinessException;
 import uo.ri.util.jdbc.Jdbc;
 
 public class DeleteMechanic {
-    private static final String TMECHANICS_DELETE = "DELETE FROM TMECHANICS " + "WHERE ID = ?";
+    private static final String TMECHANICS_DELETE = "DELETE FROM TMECHANICS "
+	+ "WHERE ID = ?";
+
+    private static final String TMECHANICHS_EXIST = "SELECT* FROM TMECHANICS WHERE ID=?";
+    private static final String TMECHANICHS_WO = "SELECT* FROM TMECHANICS WHERE ID=?";
     private String idMechanic;
 
     public DeleteMechanic(String mechanicId) {
@@ -16,10 +22,16 @@ public class DeleteMechanic {
 	this.idMechanic = mechanicId;
     }
 
-    public void execute() {
+    public void execute() throws BusinessException {
+	checkConditions();
+	executeQuery();
+    }
+
+    private void executeQuery() {
 	// Process
 	try (Connection c = Jdbc.createThreadConnection();) {
-	    try (PreparedStatement pst = c.prepareStatement(TMECHANICS_DELETE)) {
+	    try (
+		PreparedStatement pst = c.prepareStatement(TMECHANICS_DELETE)) {
 		pst.setString(1, idMechanic);
 		pst.executeUpdate();
 	    }
@@ -27,7 +39,28 @@ public class DeleteMechanic {
 	} catch (SQLException e) {
 	    throw new RuntimeException(e);
 	}
-
     }
 
+    /**
+     * @throws BusinessException if: - the mechanic does not exist - the
+     *                           mechanic has workorders assigned - the mechanic
+     *                           has interventions done - the mechanic has
+     *                           contracts
+     */
+    private void checkConditions() throws BusinessException {
+	try (Connection c = Jdbc.createThreadConnection();) {
+	    try (
+		PreparedStatement pst = c.prepareStatement(TMECHANICHS_EXIST)) {
+		pst.setString(1, idMechanic);
+
+		try (ResultSet rs = pst.executeQuery()) {
+		    if (rs.next()) {
+			throw new BusinessException();
+		    }
+		}
+	    }
+	} catch (SQLException e) {
+	    throw new RuntimeException(e);
+	}
+    }
 }
